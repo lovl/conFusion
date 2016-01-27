84d4987c8fdbc7754d1fdaf0b8052fa46df3812d
jiyange <program.ji.yang@gmail.com>
2016-01-26 19:18:51 -0700

    conFusion: Introduce the localstorage function, some sample code for further implementations

--- conFusion/www/js/controllers.js
+++ conFusion/www/js/controllers.js
@@ -2,8 +2,7 @@
 
 angular.module('conFusion.controllers', [])
 
-.controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
-
+.controller('AppCtrl', function ($scope, $ionicModal, $timeout, $localStorage) {
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
     // To listen for when this page is active (for example, to refresh data),
@@ -12,7 +11,7 @@ angular.module('conFusion.controllers', [])
     //});
 
     // Form data for the login modal
-    $scope.loginData = {};
+    $scope.loginData = $localStorage.getObject('userinfo', {});
 
     // Create the login modal that we will use later
     $ionicModal.fromTemplateUrl('templates/login.html', {
@@ -34,6 +33,7 @@ angular.module('conFusion.controllers', [])
     // Perform the login action when the user submits the login form
     $scope.doLogin = function () {
         console.log('Doing login', $scope.loginData);
+        $localStorage.storeObject('userinfo', $scope.loginData);
 
         // Simulate a login delay. Remove this and replace with your login
         // code if using a login system
